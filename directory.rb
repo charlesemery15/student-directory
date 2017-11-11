d958ae87d2594824b7d319fac10032e4b1ea9d61
charles <charlesemery15@gmail.com>
2017-11-11 14:25:57 +0000

    Step 9 - 4th interactive (control flow)

--- directory.rb
+++ directory.rb
@@ -24,7 +24,17 @@ def interactive_menu
   puts "2. Show the students"
   puts "9. Exit" # because we'll be adding more items
   # 2. read the input and save it into a variable
+  selection = gets.chomp
   # 3. do what the user has asked
+  case selection
+    when "1"
+      # input the students
+    when "2"
+      # show the students
+    when "9"
+      exit # this will cause the program to terminate
+    else
+      puts "I don't know what you meant, try again"
   end
 end
 
