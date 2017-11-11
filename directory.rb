82a90ba5d09533190c01e54bb8f1794ff6469631
charles <charlesemery15@gmail.com>
2017-11-11 16:09:21 +0000

    Load directory.rb

--- directory.rb
+++ directory.rb
@@ -4,6 +4,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit" # 9 because we'll be adding more items
 end
 
@@ -22,13 +23,14 @@ def process(selection)
     show_students
   when "3"
     save_students
+  when "4"
+    load_students
   when "9"
     exit # this will cause the program to terminate
   else
     puts "I don't know what you meant, try again"
   end
 end
-
 
 def input_students
   puts "Please entre the names of the students"
@@ -50,18 +52,6 @@ def show_students
   print_footer
 end
 
-def save_students
-  # open the file for writing
-  file = File.open("students.csv", "w")
-  # iterate over the array of students
-  @students.each do |student|
-    student_data = [student[:name], student[:cohort]]
-    csv_line = student_data.join(",")
-    file.puts csv_line
-  end
-  file.close
-end
-
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
@@ -75,6 +65,27 @@ end
 
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
+end
+
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
 end
 
 interactive_menu #replace of input_students to start with
