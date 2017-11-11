6088dc0033512350e68fc7f30a2c76bdbcbabe84
charles <charlesemery15@gmail.com>
2017-11-11 12:30:58 +0000

    Step 8, exercise 1

--- directory.rb
+++ directory.rb
@@ -23,10 +23,11 @@ def print_header
 end
 
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each_with_index do |student, index|
+    puts  "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
+
 
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
