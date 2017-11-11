3ad3b71bd92e4546fdfeaa5729421de5019e8773
charles <charlesemery15@gmail.com>
2017-11-11 14:17:13 +0000

    Step 9 - 2nd interactive (loop)

--- directory.rb
+++ directory.rb
@@ -18,10 +18,11 @@ def input_students
 end
 
 def interactive_menu
+  loop do
   # 1. print the menu and ask the user what to do
   # 2. read the input and save it into a variable
   # 3. do what the user has asked
-  # 4. repeat from step 1
+  end
 end
 
 def print_header
