dbf5bd81cb78c103d01f9c94fcea15c5e06aadbd
charles <charlesemery15@gmail.com>
2017-11-11 14:44:14 +0000

    Step 9 - 5th interactive (call)

--- directory.rb
+++ directory.rb
@@ -18,23 +18,27 @@ def input_students
 end
 
 def interactive_menu
+  students = []
   loop do
   # 1. print the menu and ask the user what to do
   puts "1. Input the students"
   puts "2. Show the students"
-  puts "9. Exit" # because we'll be adding more items
+  puts "9. Exit" # 9 because we'll be adding more items
   # 2. read the input and save it into a variable
   selection = gets.chomp
   # 3. do what the user has asked
   case selection
     when "1"
-      # input the students
+      students = input_students
     when "2"
-      # show the students
+      print_header
+      print(students)
+      print_footer(students)
     when "9"
       exit # this will cause the program to terminate
     else
       puts "I don't know what you meant, try again"
+    end
   end
 end
 
@@ -53,7 +57,7 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
-students = input_students
+interactive_menu #replace of input_students to start with
 #nothing happens until we call the methods
 print_header
 print(students)
